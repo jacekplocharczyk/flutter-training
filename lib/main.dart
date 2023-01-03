17a09d6a533dbb414786109e1cb2bea88eb080bf
Jacek Płocharczyk <jacek.plocharczyk@protonmail.com>
2023-01-03 21:50:24 +0100

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'exploration',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Hello Dude"),
+        title: const Text("Hello Dude"),
       ),
-      body: Column(children: [Progress(), TaskList()]),
-    );
-  }
-}
-
-class Progress extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: [
-      Text("What the hell"),
-      LinearProgressIndicator(
-        value: 0.9,
-      )
-    ]);
-  }
-}
-
-class TaskList extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TaskItem(
-          label: 'First article',
-        ),
-        TaskItem(label: 'Second article')
-      ],
-    );
-  }
-}
-
-class TaskItem extends StatefulWidget {
-  final String label;
-
-  const TaskItem({Key? key, required this.label}) : super(key: key);
-
-  @override
-  State<TaskItem> createState() => _TaskItemState();
-}
-
-class _TaskItemState extends State<TaskItem> {
-  bool? _value = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Checkbox(
-            value: _value,
-            onChanged: (newValue) => setState(() => _value = newValue)),
-        Text(widget.label)
-      ],
+      body: ListView(
+        children: const <Widget>[
+          ListTile(
+            leading: Icon(Icons.map),
+            title: Text('Map'),
+          ),
+          ListTile(
+            leading: Icon(Icons.photo_album),
+            title: Text('Album'),
+          ),
+          ListTile(
+            leading: Icon(Icons.phone),
+            title: Text('Phone'),
+          ),
+        ],
+      ),
     );
   }
 }
