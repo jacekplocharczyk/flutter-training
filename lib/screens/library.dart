177cd4e2457efa157c1c3ecd454d7e0f5b3f957f
Jacek Płocharczyk <jacek.plocharczyk@protonmail.com>
2023-01-04 18:29:26 +0100

    provider single files

--- lib/screens/library.dart
+++ lib/screens/library.dart
@@ -73,7 +73,6 @@ class LibraryScaffold extends StatelessWidget {
 
 class LibraryPage extends StatelessWidget {
   const LibraryPage({super.key});
-
   @override
   Widget build(BuildContext context) {
     return const LibraryScaffold();
