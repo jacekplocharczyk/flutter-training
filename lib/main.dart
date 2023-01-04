db2b4a5c86268cab689e77adc0658afcfc37b2a7
Jacek Płocharczyk <jacek.plocharczyk@protonmail.com>
2023-01-04 13:17:02 +0100

    list of items

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,66 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:training_app/common/theme.dart';
+import 'package:training_app/models/cart.dart';
+import 'package:training_app/models/catalog.dart';
+import 'package:training_app/screens/cart.dart';
+import 'package:training_app/screens/catalog.dart';
+import 'package:training_app/screens/library.dart';
 
 void main() {
   runApp(const MyApp());
+}
+
+// GoRouter router() {
+//   return GoRouter(
+//     initialLocation: '/catalog',
+//     routes: [
+//       GoRoute(
+//         path: '/catalog',
+//         builder: (context, state) => const MyCatalog(),
+//         routes: [
+//           GoRoute(
+//             path: 'cart',
+//             builder: (context, state) => const MyCart(),
+//           ),
+//         ],
+//       ),
+//     ],
+//   );
+// }
+
+List<Article> articleList = [
+  Article("title", "status 1", "some_text_content"),
+  Article("title 2", "status 2", "other some_text_content"),
+  Article("title 3", "status 3", "another some_text_content"),
+  Article("title 3", "status 3", "another some_text_content"),
+  Article("title 3", "status 3", "another some_text_content"),
+];
+
+GoRouter router() {
+  return GoRouter(
+    initialLocation: '/',
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => MyLibrary(articleList: articleList),
+        routes: [
+          GoRoute(
+            path: 'cart',
+            builder: (context, state) => const MyCart(),
+          ),
+        ],
+      ),
+    ],
+  );
 }
 
 class MyApp extends StatelessWidget {
@@ -9,38 +68,28 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'exploration',
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  const MyHomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Hello Dude"),
-      ),
-      body: ListView(
-        children: const <Widget>[
-          ListTile(
-            leading: Icon(Icons.map),
-            title: Text('Map'),
-          ),
-          ListTile(
-            leading: Icon(Icons.photo_album),
-            title: Text('Album'),
-          ),
-          ListTile(
-            leading: Icon(Icons.phone),
-            title: Text('Phone'),
-          ),
-        ],
+    // Using MultiProvider is convenient when providing multiple objects.
+    return MultiProvider(
+      providers: [
+        // In this sample app, CatalogModel never changes, so a simple Provider
+        // is sufficient.
+        Provider(create: (context) => CatalogModel()),
+        // CartModel is implemented as a ChangeNotifier, which calls for the use
+        // of ChangeNotifierProvider. Moreover, CartModel depends
+        // on CatalogModel, so a ProxyProvider is needed.
+        ChangeNotifierProxyProvider<CatalogModel, CartModel>(
+          create: (context) => CartModel(),
+          update: (context, catalog, cart) {
+            if (cart == null) throw ArgumentError.notNull('cart');
+            cart.catalog = catalog;
+            return cart;
+          },
+        ),
+      ],
+      child: MaterialApp.router(
+        title: 'Provider Demo',
+        theme: appTheme,
+        routerConfig: router(),
       ),
     );
   }
---
+++ lib/screens/library.dart
@@ -0,0 +1,221 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+import 'package:training_app/models/cart.dart';
+import 'package:training_app/models/catalog.dart';
+
+class Article {
+  Article(this.title, this.status, this.textContent);
+
+  final String title;
+  final String status;
+  final String textContent;
+}
+
+// class Article extends StatelessWidget {
+//   const Article({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return
+
+//   }
+// }
+
+class LibraryItem extends StatefulWidget {
+  final Article article;
+  const LibraryItem({Key? key, required this.article}) : super(key: key);
+
+  @override
+  State<LibraryItem> createState() => _LibraryItemState();
+}
+
+class _LibraryItemState extends State<LibraryItem> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(children: [
+      Text(widget.article.title,
+          style: Theme.of(context).textTheme.displayLarge),
+      const Spacer(),
+      Text(widget.article.status,
+          style: Theme.of(context).textTheme.displayLarge)
+    ]);
+  }
+}
+
+class MyLibrary extends StatefulWidget {
+  final List<Article> articleList;
+  const MyLibrary({Key? key, required this.articleList}) : super(key: key);
+
+  @override
+  State<MyLibrary> createState() => _MyLibraryState();
+}
+
+class _MyLibraryState extends State<MyLibrary> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Catalosssg')),
+      body: ListView.builder(
+        itemCount: widget.articleList.length,
+        itemBuilder: (context, index) {
+          return LibraryItem(article: widget.articleList[index]);
+        },
+      ),
+    );
+  }
+}
+
+
+//   Widget build2(BuildContext context) {
+//     // backing data
+//     return ListView.builder(
+//       itemCount: articles.length,
+//       itemBuilder: (context, index) {
+//         return ListTile(
+//           title: Text(articles[index].title),
+//         );
+// }
+
+
+
+// class BodyView extends StatelessWidget {
+//   const BodyView({super.key});
+
+//   final List<_Article> _articles = [
+//     _Article("News", "In progress", "Some news text")
+//   ];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return _MyListView(_articles);
+//   }
+// }
+
+// class _MyListView extends StatefulWidget {
+//   final List<_Article> articles;
+
+//   const _MyListView({Key? key, required this.articles}) : super(key: key);
+
+//   @override
+//   State<_MyListView> createState() => _MyListViewState();
+// }
+
+// class _MyListViewState extends State<_MyListView> {
+//   List<_Article> articles = [];
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // backing data
+//     return ListView.builder(
+//       itemCount: articles.length,
+//       itemBuilder: (context, index) {
+//         return ListTile(
+//           title: Text(articles[index].title),
+//         );
+//       },
+//     );
+//   }
+// }
+
+// class _AddButton extends StatelessWidget {
+//   final Item item;
+
+//   const _AddButton({required this.item});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // The context.select() method will let you listen to changes to
+//     // a *part* of a model. You define a function that "selects" (i.e. returns)
+//     // the part you're interested in, and the provider package will not rebuild
+//     // this widget unless that particular part of the model changes.
+//     //
+//     // This can lead to significant performance improvements.
+//     var isInCart = context.select<CartModel, bool>(
+//       // Here, we are only interested whether [item] is inside the cart.
+//       (cart) => cart.items.contains(item),
+//     );
+
+//     return TextButton(
+//       onPressed: isInCart
+//           ? null
+//           : () {
+//               // If the item is not in cart, we let the user add it.
+//               // We are using context.read() here because the callback
+//               // is executed whenever the user taps the button. In other
+//               // words, it is executed outside the build method.
+//               var cart = context.read<CartModel>();
+//               cart.add(item);
+//             },
+//       style: ButtonStyle(
+//         overlayColor: MaterialStateProperty.resolveWith<Color?>((states) {
+//           if (states.contains(MaterialState.pressed)) {
+//             return Theme.of(context).primaryColor;
+//           }
+//           return null; // Defer to the widget's default.
+//         }),
+//       ),
+//       child: isInCart
+//           ? const Icon(Icons.check, semanticLabel: 'ADDED')
+//           : const Text('ADD'),
+//     );
+//   }
+// }
+
+// class _MyAppBar extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return SliverAppBar(
+//       title: Text('Catalog', style: Theme.of(context).textTheme.displayLarge),
+//       floating: true,
+//       actions: [
+//         IconButton(
+//           icon: const Icon(Icons.shopping_cart),
+//           onPressed: () => context.go('/cart'),
+//         ),
+//       ],
+//     );
+//   }
+// }
+
+// class _MyListItem extends StatelessWidget {
+//   final int index;
+
+//   const _MyListItem(this.index);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     var item = context.select<CatalogModel, Item>(
+//       // Here, we are only interested in the item at [index]. We don't care
+//       // about any other change.
+//       (catalog) => catalog.getByPosition(index),
+//     );
+//     var textTheme = Theme.of(context).textTheme.titleLarge;
+
+//     return Padding(
+//       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+//       child: LimitedBox(
+//         maxHeight: 48,
+//         child: Row(
+//           children: [
+//             AspectRatio(
+//               aspectRatio: 1,
+//               child: Container(
+//                 color: item.color,
+//               ),
+//             ),
+//             const SizedBox(width: 24),
+//             Expanded(
+//               child: Text(item.name, style: textTheme),
+//             ),
+//             const SizedBox(width: 24),
+//             _AddButton(item: item),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
